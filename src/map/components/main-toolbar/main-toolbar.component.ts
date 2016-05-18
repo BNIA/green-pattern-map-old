4f5045ed4d655258ff776f24a013af27af5a0821
Ryan DeStefano <ryan.j.destefano@gmail.com>
2016-05-17 21:47:09 -0700

    Styling for the Map Toolbar

--- src/map/components/main-toolbar/main-toolbar.component.ts
+++ src/map/components/main-toolbar/main-toolbar.component.ts
@@ -1,5 +1,6 @@
 import {Component,Input,Output,EventEmitter} from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar'
+import {LayerFilters} from '../../core/layer-filters';
 import {UpgradeMDL} from '../../directives/upgrade-mdl'
 
 @Component({
@@ -14,5 +15,9 @@ export class MainToolbarComponent{
     @Output() menuButtonClicked:EventEmitter<any> = new EventEmitter()
     fireMenuButtonClicked(){
         this.menuButtonClicked.emit(null)
+    }
+    public search = false;
+    showSearch() {
+      this.search = !this.search;
     }
 }
